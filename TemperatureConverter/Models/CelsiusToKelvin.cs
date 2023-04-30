de17d3028b0a765225316d0aa21ee15e370d017e
Emzzy241 <emzzyoluwole@gmail.com>
2023-04-30 10:38:28 +0100

    adding my Celsius to kelvin file

---
+++ TemperatureConverter/Models/CelsiusToKelvin.cs
@@ -0,0 +1,12 @@
+// I created a new class just so I can be able to differentiate the KelvinToCelsius class and the CelsiusTOKelvin class files
+
+// Here I would be taking in a Celsius Temperature from my user and converting it into a Kelvin Temperature
+// This would basically just be like the opposite of what we did in the KelvinToCelsius c-sharp file
+
+namespace TemperatureConverter.Models
+{
+    public class CelsiusTOKelvin
+    {
+        public double ConvertingCelsiusToKelvin
+    }
+}
