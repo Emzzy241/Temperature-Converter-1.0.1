06dbf2dac7d85daad0e143d8abbedd236375fc61
Emzzy241 <emzzyoluwole@gmail.com>
2023-05-01 05:16:27 +0100

    adding my Fahrenheit To celsius C# file

--- TemperatureConverter/Models/FahrenheitToCelsius.cs
+++ TemperatureConverter/Models/FahrenheitToCelsius.cs
@@ -1,6 +1,7 @@
 // This is the class file for the conversion of Fahrenheit to Celsius Temperature
 
 using System;
+using System.Collections.Generic;
 
 
 // a method that takes in a fahrenheit Temperature and converts to celsius temperature
@@ -9,14 +10,42 @@ namespace TemperatureConverter.Models
 {
 
 
-    class FahrenheitToCelsius
+    public class FahrenheitToCelsius
     {
-        // public string FahrenheitToCelsius()
-        // {
+        public double StillConvertingFahrenheitToCelsius { get; set; }
+        public double ConvertingfahrenheitToCelsius { get; set; }
 
-        // }
+        private static List<FahrenheitToCelsius> _thirdInstances = new List<FahrenheitToCelsius>();
 
-        // WRITING A PUBLIC METHOD THAT WILL ADD
+        public List<FahrenheitToCelsius> ForMyFahrenheitToCelsiusTemperatureList
+        {
+            get { return _thirdInstances; }
+            set { _thirdInstances = value; }
+        }
+
+        // my constructor that helps me to handle the different C# properties I've written
+
+        public FahrenheitToCelsius(double fahrenheitTemperature, double standardOfFahrenheitoCelsius)
+        {
+            StillConvertingFahrenheitToCelsius = fahrenheitTemperature;
+            ConvertingfahrenheitToCelsius = standardOfFahrenheitoCelsius;
+            _thirdInstances.Add(this);
+        }
+
+        public double FahrenheitConverterMethod()
+        {
+
+            if ((StillConvertingFahrenheitToCelsius < 0) && (ConvertingfahrenheitToCelsius == 0))
+            {
+                throw new ArgumentException("Invalid temperature in kelvin");
+            }
+            else{
+                double finalFahrenheitValue = StillConvertingFahrenheitToCelsius;
+                return finalFahrenheitValue;
+            }
+        }
+
+
     }
 
 
