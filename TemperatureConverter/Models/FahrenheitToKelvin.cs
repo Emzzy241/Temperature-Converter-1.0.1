7c0980aed0cdb5fad85729575576a6961414b2b2
Emzzy241 <emzzyoluwole@gmail.com>
2023-05-02 04:45:28 +0100

    ading all and pushing

---
+++ TemperatureConverter/Models/FahrenheitToKelvin.cs
@@ -0,0 +1,55 @@
+// This is the class file for the conversion of Fahrenheit to Celsius Temperature
+
+
+// THIS IS THE FORMAT FOR THIS c# FILE, ALTHOUGH i NEED TO STILL UPDATE IT 
+// using System;
+// using System.Collections.Generic;
+
+
+// // a method that takes in a fahrenheit Temperature and converts to celsius temperature
+
+// namespace TemperatureConverter.Models
+// {
+
+
+//     public class FahrenheitToCelsius
+//     {
+//         public double StillConvertingFahrenheitToCelsius { get; set; }
+//         public double ConvertingfahrenheitToCelsius { get; set; }
+
+//         private static List<FahrenheitToCelsius> _thirdInstances = new List<FahrenheitToCelsius>();
+
+//         public List<FahrenheitToCelsius> ForMyFahrenheitToCelsiusTemperatureList
+//         {
+//             get { return _thirdInstances; }
+//             set { _thirdInstances = value; }
+//         }
+
+//         // my constructor that helps me to handle the different C# properties I've written
+
+//         public FahrenheitToCelsius(double fahrenheitTemperature, double standardOfFahrenheitoCelsius)
+//         {
+//             StillConvertingFahrenheitToCelsius = fahrenheitTemperature;
+//             ConvertingfahrenheitToCelsius = standardOfFahrenheitoCelsius;
+//             _thirdInstances.Add(this);
+//         }
+
+//         public double FahrenheitConverterMethod()
+//         {
+
+//             if ((StillConvertingFahrenheitToCelsius < 0))
+//             {
+//                 throw new ArgumentException("Invalid temperature in kelvin");
+//             }
+//             else{
+//                 double finalFahrenheitValue = StillConvertingFahrenheitToCelsius;
+//                 return finalFahrenheitValue;
+//             }
+//         }
+
+
+//     }
+
+
+// }
+
