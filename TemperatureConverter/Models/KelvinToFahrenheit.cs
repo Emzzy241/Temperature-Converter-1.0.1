a854e67a7684cf6ce9923e908cbe4eca9869ab63
Emzzy241 <emzzyoluwole@gmail.com>
2023-05-10 17:57:18 +0100

    updating business logic code

--- TemperatureConverter/Models/KelvinToFahrenheit.cs
+++ TemperatureConverter/Models/KelvinToFahrenheit.cs
@@ -5,8 +5,8 @@ namespace TemperatureConverter.Models
 {
     public class KelvinToFahrenheit
     {
-        double ConvetingKelvinToFahrenheit { get; set; }
-        double StillConvetingKelvinToFahrenheit { get; set; }
+        double StillConvertingKelvinToFahrenheit { get; set; }
+        double ConvertingKelvinToFahrenheit { get; set; }
 
         private static List<KelvinToFahrenheit> _sixthInstance  = new List<KelvinToFahrenheit>();
 
@@ -18,15 +18,15 @@ namespace TemperatureConverter.Models
 
         public KelvinToFahrenheit(double myKelvinTemperature, double myStandardsOfTemperature)
         {
-            StillConvetingKelvinToFahrenheit = myKelvinTemperature;
-            ConvetingKelvinToFahrenheit = myStandardsOfTemperature;
+            StillConvertingKelvinToFahrenheit = myKelvinTemperature;
+            ConvertingKelvinToFahrenheit = myStandardsOfTemperature;
             _sixthInstance.Add(this);
         }
 
         public double SecondKelvinToFahrenheitConverterMethod()
         {
-            double trueConversion = (ConvetingKelvinToFahrenheit - 273.15) * 1.8 + 32;
-            if(StillConvetingKelvinToFahrenheit < 0)
+            double trueConversion = (ConvertingKelvinToFahrenheit - 273.15) * 1.8 + 32;
+            if(StillConvertingKelvinToFahrenheit < 0)
             {
                 throw new ArgumentException("Invalid temperature value");
             }
